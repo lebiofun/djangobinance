adc1a90b0e0cd8ba369ef5b34a5486e37e6d184a
lebiofun <85184445+lebiofun@users.noreply.github.com>
2026-09-18 08:18:20 +0300

    Use environment variable for SECRET_KEY
    Replaced hardcoded SECRET_KEY with environment variable.

--- binance_project/settings.py
+++ binance_project/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh7^l^^#_@(@h%eewkm#c2t33b*&m0qi=tfa-qy7vcof*9wjoq'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise RuntimeError("DJANGO_SECRET_KEY is not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
